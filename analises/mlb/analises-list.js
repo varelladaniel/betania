a3712850c15444e96412158026d9e35b9a3cdc3e
Daniel Varella <danielvarella@gmail.com>
2026-08-02 15:03:06 -0300

    Adiciona análise MLB: Minnesota Twins x Seattle Mariners (02/08/2026)

--- analises/mlb/analises-list.js
+++ analises/mlb/analises-list.js
@@ -1,4 +1,5 @@
 window.MLB_ANALISES = [
+  { slug: "2026-08-02_minnesota-twins-x-seattle-mariners", dataLabel: "02/08", timeLabel: "17:10", teamsLabel: "Minnesota Twins vs Seattle Mariners" },
   { slug: "2026-08-01_stl-cardinals-x-tor-blue-jays", dataLabel: "01/08", timeLabel: "16:07", teamsLabel: "St. Louis Cardinals vs Toronto Blue Jays" },
   { slug: "2026-08-01_chicago-white-sox-x-tampa-bay-rays", dataLabel: "01/08", timeLabel: "17:10", teamsLabel: "Chicago White Sox vs Tampa Bay Rays" },
   { slug: "2026-08-01_minnesota-twins-x-seattle-mariners", dataLabel: "01/08", timeLabel: "17:10", teamsLabel: "Minnesota Twins vs Seattle Mariners" },
